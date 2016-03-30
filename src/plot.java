db032ba3ea728094956a0852cf4fc986d7ed7c88
ambhautik11 <amipara.bhautik11@gmail.com>
2016-03-30 21:15:58 +0530

    1
    1

---
+++ src/plot.java
@@ -0,0 +1,13 @@
+
+public class plot {
+
+	public static void main(String[] args)
+	{
+		
+		for(int i =1 ;i<5;i++)
+		{
+			SimulatedAnnealing s = new SimulatedAnnealing();
+			s.start();
+		}
+	}
+}
